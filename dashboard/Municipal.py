004e64a022c7da69e4630d236676220982ecf8d4
Your Name <you@example.com>
2018-04-14 13:17:17 +0200

    added routes

---
+++ dashboard/Municipal.py
@@ -0,0 +1,8 @@
+# This is our domain object - use this
+{
+region_id = 213
+jobs = [job1, job2, job3]
+recommendation_score = 0.723
+konkurens =
+livs =
+}
---
+++ dashboard/job.py
@@ -0,0 +1,1 @@
+# This is our domain object - use this
