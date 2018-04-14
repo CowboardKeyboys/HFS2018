eaaef309b19276b267e266e0229720037d7e3e9c
cjoh88 <cjoh193@gmail.com>
2018-04-14 20:07:00 +0200

    added training data method

--- dashboard/static/data/database.py
+++ dashboard/static/data/database.py
@@ -1,6 +1,7 @@
 import json
 import time
 from job_listing import job_listing
+import numpy as np, scipy.stats as st
 
 class database:
 
@@ -29,6 +30,18 @@ class database:
 		
 	def get_all_jobs(self):
 		return json.dumps([jl.__dict__ for jl in self.data.values()])
+		
+	def get_training_data(self):
+		desc = []
+		id = []
+		a = [len(t.location_desc) for t in self.data.values()[:100:]]
+		leftQ, rightQ = st.t.interval(0.99, len(a)-1, loc=np.mean(a), scale=st.sem(a))
+		median = np.median(a)
+		for line in self.data.values():
+			if len(line.location_desc)>leftQ and len(line.location_desc)<rightQ:
+				desc.append(line.location_desc)
+				id.append(line.id)
+		return desc, id
 				
 	def pr(self):
 		for k, v in self.data.iteritems():
@@ -37,13 +50,16 @@ class database:
 				
 				
 def test():
-	db = database("500000.json")
+	db = database("100000.json")
 	#print db.get_job_from_id('0017-482230').to_json()
 	#print db.get_jobs_in_region('0883')
 	#print db.get_all_jobs()
-	print db.get_jobs_from_ids(["0017-544186", "0017-627299", "0017-667999"])
+	#print db.get_jobs_from_ids(["0017-544186", "0017-627299", "0017-667999"])
 	#print "data loaded."
 	#time.sleep(20000)
+	x,y = db.get_training_data()
+	print str(len(x))
+	print str(len(y))
 	
 	
 if __name__ == "__main__":
